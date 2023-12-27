355f19449cc9bd7c57bd622a2319ea2a136b4d81
anfal11 <anfal15-14303@diu.edu.bd>
2023-12-28 01:46:55 +0600

    singlePropertyDeatils done 40%

--- src/Pages/PropertyDetails.jsx
+++ src/Pages/PropertyDetails.jsx
@@ -30,35 +30,36 @@ const PropertyDetails = () => {
       <h3 className="text-4xl my-10 font-semibold text-gray-600">
             {property?.title}
           </h3>
-        <div className="mb-20 group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl ">
+        <div className="mb-20 group flex flex-col h-full bg-white rounded-xl ">
         
-      <div className="h-52 flex flex-col justify-center items-center bg-blue-600 rounded-t-xl">
+      <div className="flex flex-col justify-center items-center bg-blue-600 rounded-t-xl">
       
-        <img className="h-52 object-cover w-full" src={property?.image} alt={property?.title}  />
+        <img className="object-cover w-full" src={property?.image} alt={property?.title}  />
       </div>
 
       <div className="p-4 md:p-6">
-        
-          
-          <span className="block mb-1 mt-3 text-xs font-semibold uppercase text-blue-600">
-            {property?.location}
-          </span>
-          <p className="mt-3 text-gray-600 font-bold">
-            Type: {property?.type}
-          </p>
-          <p className="mt-3 text-gray-600 font-semibold">
-            ${property?.price}
-          </p>
-          {
+      {
               property?.status === 'For Sale' ? <p className="mt-3 text-gray-600 font-extrabold bg-yellow-400 w-24 p-4 text-center rounded-e-full">
             {property?.status}
           </p> : <p className="mt-3 text-white font-bold bg-blue-600 w-24 p-4 text-center rounded-e-full">
               {property?.status}
               </p>
           }
-          {/* <p className="mt-3 text-gray-600 font-bold bg-yellow-400 w-24 p-4 text-center rounded-e-full">
-            {status}
-          </p> */}
+          
+          <span className="block mb-1 mt-3 text-3xl font-semibold uppercase text-blue-600">
+            {property?.location}
+          </span>
+          <p className="mt-3 text-gray-600 text-xl font-bold">
+           <span className='underline'>About this {property?.type}:</span> {property?.description}
+          </p>
+          <p className="mt-3 text-gray-600 text-xl font-bold">
+            Type: {property?.type}
+          </p>
+          <p className="mt-3 text-gray-600 text-xl font-semibold">
+            ${property?.price}
+          </p>
+         
+         <button onClick={()=> window.history.back()} className='btn mt-10 bg-blue-600 text-white hover:bg-blue-400'> Go Back </button>
         </div>
 
       </div>
