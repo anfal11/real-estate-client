39455032f47d242c1b65ecfa259e58be30eea4b2
anfal11 <anfal15-14303@diu.edu.bd>
2023-12-29 17:21:26 +0600

    useAdmin and useAxiosSecure creared

---
+++ src/Hooks/useAxiosSecure.jsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
+
+
+const axiosSecure = axios.create({
+    baseURL: 'http://localhost:5000/'
+})
+const useAxiosSecure = () => {
+    const {logOut} = useAuth();
+    const navigate = useNavigate();
+    // request interceptor to add authorization header for every secure call to the api
+    axiosSecure.interceptors.request.use(
+        (config) => {
+            const token = localStorage.getItem("access token");
+            if (token) {
+                config.headers.authorization = `Bearer ${token}`;
+            }
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
+        }
+    );
+
+
+    //intercepts 401 and 403
+    axiosSecure.interceptors.response.use(function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response;
+      }, function (error) {
+        //for 401 or 403 logout the user and move the user to the login page
+            if(error.response.status === 401 || error.response.status === 403) {
+                localStorage.removeItem("access token");
+                // window.location.href = "/login";
+                logOut();
+                navigate("/login");
+            }
+        return Promise.reject(error);
+      });
+    return axiosSecure;
+}
+
+export default useAxiosSecure;
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,10 +1,27 @@
-
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Outlet } from "react-router-dom";
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
+        <div className="drawer lg:drawer-open">
+  <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content flex items-center">
+    {/* Page content here */}
+    <label htmlFor="my-drawer-2" ><GiHamburgerMenu className="text-4xl m-4 lg:hidden" /></label>
+        
+        <Outlet />
+  
+  </div> 
+  <div className="drawer-side">
+    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
+    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+      {/* Sidebar content here */}
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+    </ul>
+  
+  </div>
+</div>
     );
 };
 
